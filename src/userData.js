4ded2b26b5f6ff1b6ad9a3c9a0c202652105148e
Jake Higgs <jakehiggs@Jakes-MacBook-Pro.local>
2019-01-31 18:07:38 -0700

    added the data

---
+++ src/userData.js
@@ -0,0 +1,33 @@
+export default userData = [
+    {
+        name: 'Tom Smith',
+        age: 32,
+        email: 'thebest@gmail.com'
+    },
+    {
+        name: 'Amy Smith',
+        age: 23,
+        email: 'theotherbest@gmail.com'
+    },
+    {
+        name: 'Robert Fleck',
+        age: 50,
+        email: 'notthebest@gmail.com'
+    },
+    {
+        name: 'Gretchen Lewandowski',
+        age: 20,
+        email: 'glewan@gmail.com'
+    },
+    {
+        name: 'Jerry Hudson',
+        age: 34,
+        email: 'jhud395@gmail.com'
+    },
+    {
+        name: 'Coral Bonner',
+        age: 39,
+        email: 'cBonner@gmail.com'
+    },
+
+]
